4266ce918924170a73a10974f7aa2be34028f281
icewizzard <yoniroit12310@gmail.com>
2023-03-21 13:51:33 +0200

    new commit

---
+++ bankproject002/app.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user, LoginManager, UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
+from flask_sqlalchemy import SQLAlchemy
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+app.secret_key = 'Ak47'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://yoni:yoni123@localhost:5432/bank'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Create a user loader function
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/protected")
+@login_required
+def protected_route():
+    return "This is a protected route that only logged-in users can access"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('index'))
+        flash('Invalid email or password')
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=generate_password_hash(form.password.data))
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created successfully!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
